08b98d0c070af16aab01eed9b2d7869b41475380
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-05-01 21:33:10 +0300

    Update Point.hpp

--- Point.hpp
+++ Point.hpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #include <iostream>
 #include <string>
@@ -12,25 +11,6 @@ class Point{
         int y;
         Point();
         Point(int num1,int num2);
-        friend ostream& operator<<(ostream& out, const Point& p);
+     
         ~Point();
 };
-=======
-
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-class Point{
-
- 
-    public:
-     string pair;
-        int x;
-        int y;
-        Point();
-        Point(int num1,int num2);
-        ~Point();
-};
->>>>>>> 71851d128dbe88c4e57541b698dd488dcc4ecd4c
