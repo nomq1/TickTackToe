b458266fb36119044ca62e6aecc38ba0638e397d
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-05-01 21:30:59 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,8 +1,6 @@
-
 #include "Board.hpp"
-
 #include <iostream>
-#include<vector>
+
 using namespace std;
 
     Board::Board() //ok
@@ -15,31 +13,41 @@ using namespace std;
     Board::Board(int inputsize) //ok
     {
         this->size=inputsize;
+        
         this->board=new char[inputsize*inputsize];
         for(int i=0;i<size*size;i++)
-        {
+        {          
             board[i-1]='.';
+
         }
+        
     }
 
-    char& Board::operator[](Point po) //should recieve {x,y}  ????? 
+   char Board::getValue(int M, int N)
+     {
+         int s=(this->size);
+         int num=M*size; 
+         return this->board[num+N]; 
+     }
+
+    char& Board::operator[](Point po) //ok
      { 
          int s=(this->size);
          int num=(po.x)*size; 
          return this->board[num+po.y];     
      }
 
-    ostream& operator<<(ostream& out, const Board& b){
+
+
+    ostream& operator<<(ostream& out,Board &b){
     for(int i=0;i<b.size;i++)
-    {
+    {    
         for(int j=0;j<b.size;j++)
         {
-         Point p(i,j);
-        // out<< b.board[p];
-         out<<' ';
+         out<<b.getValue(i,j);
         }
         out<<"\n";
-    }  
+    } 
     return out;
     }
 
@@ -52,58 +60,3 @@ using namespace std;
 
    
 
-=======
-#include "Board.hpp"
-
-#include <iostream>
-#include<vector>
-using namespace std;
-
-    Board::Board()
-    {
-        this->size=0;
-        char board[NULL][NULL];
-    }
-
-
-    Board::Board(int inputsize)
-    {
-        this->size=inputsize;
-        this->board=new char[inputsize*inputsize];
-        for(int i=0;i<size*size;i++)
-        {
-            board[i-1]='.';
-        }
-    }
-
-    char& Board::operator[](Point po)
-     { 
-         int s=(this->size);
-         int num=(po.x)*size;
-         return this->board[num+po.y];     
-     }
-
-
-    ostream& operator<<(ostream& out, const Board& b){
-    vector<int> vecRow;
-    for (int i = 0; i <(b.size); i++){
-        for (int j = 0; j < (b.size); j++){
-            Point p(i,j);
-          //  out <<*(b.board)[p]; //צריך לסדר את זה!! להכניס את המיקום להדפסה
-            out <<"\t";
-        }
-        out << endl;
-    }
-    return out;
-    }
-
-
-    Board::~Board()
-    {
-        cout<<"";
-    } 
-
-
-   
-
->>>>>>> 71851d128dbe88c4e57541b698dd488dcc4ecd4c
