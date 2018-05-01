f61d345ef55cad505cc0855836faa5a1f902228f
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-05-01 21:32:31 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #include <iostream>
 #include "Point.hpp"
@@ -20,16 +19,7 @@ using namespace std;
             this->pair='{'+num1+','+num2+'}';
         }
 
-        ostream& operator<<(ostream& out, const Point& p)
-        {
-            out<<'{';
-            out<<p.x;
-            out<<',';
-            out<<p.y;
-            out<<'}';
-
-           return out;
-        }
+        
 
         
         Point::~Point()
@@ -41,36 +31,3 @@ using namespace std;
 
        
 
-=======
-
-#include <iostream>
-#include "Point.hpp"
-
-using namespace std;
-
-
-        Point::Point()
-        {
-            this->x=-1;
-            this->y=-1;
-            this->pair="{null,null}";
-        }
-
-        Point::Point(int num1,int num2)
-        {
-            this->x=num1;
-            this->y=num2;
-            this->pair=string("{"+x+','+y+'}');
-        }
-
-        
-        Point::~Point()
-        {
-            cout<<"";
-        }
-
-
-
-       
-
->>>>>>> 71851d128dbe88c4e57541b698dd488dcc4ecd4c
