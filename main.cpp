2a8ae823a42cc7199b3d898aab38427259353f0f
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-05-06 21:17:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 #include "Board.hpp"
 #include <iostream>
-using namespace std;
-
 using namespace std;
 
 int main() {
@@ -13,26 +10,19 @@ int main() {
 	....
 	....
 	*/
+	cout << board1[{1,2}] << endl; // .
 	board1[{1,1}]='X';
 	board1[{1,2}]='O';
+	char c = board1[{1,2}]; cout << c << endl; // O
 	cout << board1 << endl;  /* Shows the following board:
 	....
 	.XO.
 	....
 	....
 	*/
-
+	try {
+	board1[{3,4}]='O';   // This should raise an exception
+	} catch (const IllegalCoordinateException& ex) {
+		cout << "Illegal coordinate: " << ex.theCoordinate() << endl;  // prints "Illegal coordinate: 3,4"
+	}
 }
-=======
-#include "Board.hpp"
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    Board b(4);
-    Point p(3,3);
-    cout<<b[p];
-
-}
->>>>>>> 71851d128dbe88c4e57541b698dd488dcc4ecd4c
