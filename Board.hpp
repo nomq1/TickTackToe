9e2e316f549b962121d28d0442122714a790050d
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-05-08 20:10:46 +0300

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -4,20 +4,33 @@
 
 using namespace std;
 
+class Node{
 
+        public:
+        char ch;
+        Node();
+        Node(char c);
+        void setch(char c);
+        Node& operator=(char c);
+        operator char()const{
+                return ch;
+        }
+};
 
 class Board{
  
    public:
         int size;
-        char *board;
+        Node *board;
         Board();
         Board(int inputsize);
         char getValue(int M, int N);   
         char& operator[](Point po);
         void operator=(char c);
+        void operator=(Board& b);
         friend ostream& operator<<(ostream& out, Board &b); 
         ~Board();  
 
 };
+
 
