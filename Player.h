4d4594bdeb6e652253bbf56019f46bb5845534b3
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-05-21 14:21:23 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,14 +1,53 @@
-#include <string>
-#include "Board.h"
-using Coordinate=Point;
+#pragma once
+#include "Player.h"
 
-class Player{
-    private:
 
-    public:
-    char myChar; //save the player char: X or O
-    char getchar(){ return myChar; }
 
-    virtual const string name() const=0;
-	virtual const Coordinate play(const Board& board)=0;
+/* 
+	This player scans the board looping on x then on y, 
+	and plays on the first empty cell. 
+*/
+class XYPlayer: public Player {
+public:
+	const string name() const override { return "XYPlayer"; }
+
+	/**
+	 * Selects a single place to put my symbol, given the current board.
+	 * Defines the strategy of the player.
+	 */
+	const Coordinate play(const Board& board) override;
+};
+
+
+/* 
+	This player scans the board looping on y then on x, 
+	and plays on the first empty cell. 
+*/
+class YXPlayer: public Player {
+public:
+	const string name() const override { return "YXPlayer"; }
+	const Coordinate play(const Board& board) override;
+};
+
+
+/* 
+   This player makes illegal moves - 
+   it tries to override cells of the other player.
+   It should always lose.
+*/
+class IllegalPlayer: public Player {
+public:
+	const string name() const override { return "YXPlayer"; }
+	const Coordinate play(const Board& board) override;
+};
+
+
+/* 
+	This player always throws an exception.
+	It should always lose. 
+*/
+class ExceptionPlayer: public Player {
+public:
+	const string name() const override { return "YXPlayer"; }
+	const Coordinate play(const Board& board) override;
 };
