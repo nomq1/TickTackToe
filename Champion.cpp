e69828e716b5498c16e453b4f047d460e769bff4
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-05-22 19:21:51 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -5,15 +5,7 @@ using Coordinate=Point;
 const Coordinate Champion::play(const Board& board) {
     
 
-    for (int x=0; x<board.size(); ++x) {
-        Coordinate c{board.size()-1-x,x};
-        if(x==1 && board[{0,board.size()-1}] == '.')
-            c.setXY(0,board.size()-1);
-		if(board[c]=='.') {
-			return c;
-        }
-		
-	}
+	
 	return {0,0};
 
 }
