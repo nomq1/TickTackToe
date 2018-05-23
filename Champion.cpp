89cf7d613710748104a6aeebe51def2da7a853ae
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-05-23 20:26:49 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -2,18 +2,9 @@
 using Coordinate=Point;
 
 
-const Coordinate Champion::play(const Board& board) {
-    
+const Coordinate Champion::play(const Board& board) //1 turn of champion player 
+ {
 
-    for (int x=0; x<board.size(); ++x) {
-        Coordinate c{board.size()-1-x,x};
-        if(x==1 && board[{0,board.size()-1}] == '.')
-            c.setXY(0,board.size()-1);
-		if(board[c]=='.') {
-			return c;
-        }
-		
-	}
-	return {0,0};
+return {0,0};
 
-}
+ }
