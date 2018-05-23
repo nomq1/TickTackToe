4b7c615271b35e857868f0da414c5a4786fea65f
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-05-23 20:28:23 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,7 +1,7 @@
 
 #include <iostream>
 #include <string>
-#pragma once
+
 using namespace std;
 
 class Point{
@@ -11,6 +11,6 @@ class Point{
         int y;
         Point();
         Point(int num1,int num2);
-        void setXY(int x, int y);
+     
         ~Point();
 };
