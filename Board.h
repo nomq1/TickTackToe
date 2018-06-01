1c0e46c3e8f404269f2d3da0cdc8c502e466a38a
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-06-01 09:17:23 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,10 +1,26 @@
-#include <iostream>
-#pragma once
 
+#pragma once
+#include <iostream>
 #include "IllegalCoordinateException.h"
 #include "IllegalCharException.h"
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <time.h>
+
+
+static int FileNum = 1; 
+
+struct RGB {
+  uint8_t red, green, blue;
+public:
+  RGB() {}
+  RGB(uint8_t red, uint8_t green, uint8_t blue): red(red), green(green), blue(blue) {}
+};
+
 
 using namespace std;
+
 
 class Node{
 
@@ -34,6 +50,9 @@ class Board{
         char operator[](Point po) const;
         void operator=(char c);
         friend ostream& operator<<(ostream& out, const Board &b);
+        friend istream& operator>>(istream& input,Board &b);
+        string draw (const int pix) const;
+        void drawTable(RGB* image, int pix) const;
         void deleteB();
         void operator=(const Board& b);
         ~Board();
